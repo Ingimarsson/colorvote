98bbb1d78ef24b605ea7fb27efe15034dc9a2fae
Brynjar Ingimarsson <brynjar@ingimarsson.is>
2020-07-17 22:46:20 +0000

    added API fetch for results and tx page

--- web/src/components/pages/TransactionsPage.js
+++ web/src/components/pages/TransactionsPage.js
@@ -1,7 +1,41 @@
 import React, { Component } from 'react';
-import { Container, Table } from 'semantic-ui-react';
+import { Container, Table, Label } from 'semantic-ui-react';
+
+import moment from 'moment';
 
 class TransactionsPage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.intervalID = 0;
+
+    this.state = {
+      transactions: [],
+    }
+  }
+
+  componentDidMount() {
+    this.loadData();
+
+    this.intervalID = setInterval(this.loadData.bind(this), 5000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalID);
+  }
+
+  loadData() {
+    var that = this;
+    
+    fetch('/transactions')
+      .then(response => response.json())
+      .then(function(data) {
+        that.setState({
+          transactions: data 
+        });
+      });
+  }
+
   render() {
     return (
       <Container>
@@ -9,21 +43,30 @@ class TransactionsPage extends Component {
         <Table>
           <Table.Header>
             <Table.Row>
+              <Table.HeaderCell>Type</Table.HeaderCell>
+              <Table.HeaderCell>Election</Table.HeaderCell>
               <Table.HeaderCell>Timestamp</Table.HeaderCell>
               <Table.HeaderCell>Height</Table.HeaderCell>
-              <Table.HeaderCell>Election</Table.HeaderCell>
               <Table.HeaderCell>Receiver</Table.HeaderCell>
               <Table.HeaderCell>Votes</Table.HeaderCell>
             </Table.Row>
           </Table.Header>
           <Table.Body>
-            <Table.Row>
-              <Table.Cell>Jul 4, 2020 7:57:44 PM</Table.Cell>
-              <Table.Cell>701442</Table.Cell>
-              <Table.Cell><a href='#'>B77xG33a6ogDJpX4PgoM8qFTFvhWnRrUBY</a></Table.Cell>
-              <Table.Cell>50</Table.Cell>
-              <Table.Cell><a href='#'>B77xG33a6ogDJpX4PgoM8qFTFvhWnRrUBY</a></Table.Cell>
-            </Table.Row>
+            {this.state.transactions.map(transaction => 
+              <Table.Row>
+                <Table.Cell>
+                  { transaction.type == 'issue' ?
+                    <Label size='tiny' color='blue'>Issue</Label> :
+                    <Label size='tiny'>Transfer</Label>
+                  }
+                </Table.Cell>
+                <Table.Cell>{transaction.election}</Table.Cell>
+                <Table.Cell>{moment.unix(transaction.time).format('MMMM Do YYYY, h:mm:ss a')}</Table.Cell>
+                <Table.Cell>{transaction.block}</Table.Cell>
+                <Table.Cell>{transaction.address}</Table.Cell>
+                <Table.Cell>{transaction.amount}</Table.Cell>
+              </Table.Row>
+            )}
           </Table.Body>
         </Table>
       </Container>
