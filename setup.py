8776fc4318c8883c5e9a322f7f4bcd9a30e6c8fd
Brynjar Ingimarsson <brynjar@ingimarsson.is>
2020-07-17 10:03:15 +0000

    Added package to PyPI

--- setup.py
+++ setup.py
@@ -1,14 +1,22 @@
 from setuptools import setup
 
-setup(name='colorvote',
-      version='0.1',
-      description='A package for the colored coins voting protocol',
-      url='http://github.com/Ingimarsson/colorvote',
-      author='Brynjar Ingimarsson',
-      author_email='brynjar@ingimarsson.is',
-      license='MIT',
-      packages=['colorvote'],
-      install_requires=[
-          'requests',
-      ],
-      zip_safe=False)
+with open('README.md', 'r') as fh:
+  long_description = fh.read()
+
+setup(
+  name='colorvote',
+  version='0.1',
+  description='A package for the colored coins voting protocol',
+  url='http://github.com/Ingimarsson/colorvote',
+  author='Brynjar Ingimarsson',
+  author_email='brynjar@ingimarsson.is',
+  long_description=long_description,
+  long_description_content_type='text/markdown',
+  license='MIT',
+  packages=['colorvote'],
+  install_requires=[
+    'requests',
+  ],
+  zip_safe=False,
+  python_requires='>=3.6'
+)
