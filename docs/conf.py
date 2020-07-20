2471c762d5fc9f37577fc053e9d50e1d20a950dc
Brynjar Ingimarsson <brynjar@ingimarsson.is>
2020-07-20 16:00:54 +0000

    changed sphinx conf for rtd

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,9 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('../../colorvote/'))
+sys.path.insert(0, os.path.abspath('..'))
+
+import colorvote
 
 # -- Project information -----------------------------------------------------
 
